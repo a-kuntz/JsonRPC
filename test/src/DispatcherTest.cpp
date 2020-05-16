1c213c1bc2f6b63398ac346e75bdeb05181d9727
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2020-05-16 21:58:27 +0200

    add spec examples as comments

--- test/src/DispatcherTest.cpp
+++ test/src/DispatcherTest.cpp
@@ -168,9 +168,57 @@ TEST_F(DispatcherTest, SpecificationExamples)
 		rx(R"({"jsonrpc": "2.0", "method": 1, "params": "bar"})"),
 		tx(R"({"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null})"));
 
+	// rpc call Batch, invalid JSON:
+	// --> [
+	//   {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+	//   {"jsonrpc": "2.0", "method"
+	// ]
+	// <-- {"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error"}, "id": null}
+	// ASSERT_EQ(
+	// 	rx(R"()"),
+	// 	tx(R"()"));
+
 	// rpc call with an empty Array:
 	// --> []
 	// <-- {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null}
 	ASSERT_EQ(
 		rx(R"([])"), tx(R"({"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null})"));
+
+	// rpc call with an invalid Batch (but not empty):
+	// --> [1]
+	// <-- [
+	//   {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null}
+	// ]
+
+	// rpc call with invalid Batch:
+	// --> [1,2,3]
+	// <-- [
+	//   {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null},
+	//   {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null},
+	//   {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null}
+	// ]
+
+	// rpc call Batch:
+	// --> [
+	//         {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+	//         {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
+	//         {"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},
+	//         {"foo": "boo"},
+	//         {"jsonrpc": "2.0", "method": "foo.get", "params": {"name": "myself"}, "id": "5"},
+	//         {"jsonrpc": "2.0", "method": "get_data", "id": "9"}
+	//     ]
+	// <-- [
+	//         {"jsonrpc": "2.0", "result": 7, "id": "1"},
+	//         {"jsonrpc": "2.0", "result": 19, "id": "2"},
+	//         {"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null},
+	//         {"jsonrpc": "2.0", "error": {"code": -32601, "message": "Method not found"}, "id": "5"},
+	//         {"jsonrpc": "2.0", "result": ["hello", 5], "id": "9"}
+	//     ]
+
+	// rpc call Batch (all notifications):
+	// --> [
+	//         {"jsonrpc": "2.0", "method": "notify_sum", "params": [1,2,4]},
+	//         {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]}
+	//     ]
+	// <-- //Nothing is returned for all notification batches
 }
