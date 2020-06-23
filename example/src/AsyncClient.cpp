738aee699f333ef2cb4d7ba24be283b2955583f5
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2020-06-23 23:24:48 +0200

    clang format

--- example/src/AsyncClient.cpp
+++ example/src/AsyncClient.cpp
@@ -24,11 +24,10 @@ using Completion = std::function<void(const ResultType&)>;
 class Session : public std::enable_shared_from_this<Session>
 {
 private:
-	tcp::socket&              _socket;
-	std::string              _buffer;
+	tcp::socket& _socket;
+	std::string  _buffer;
 
 public:
-
 	Session(boost::asio::io_context& ioc, tcp::socket& socket)
 		: _socket(socket)
 	{}
@@ -37,7 +36,7 @@ public:
 	{
 		static int id = 0;
 
-        _buffer = Json(Request{"2.0", name, args, std::to_string(++id)}).dump();
+		_buffer = Json(Request{"2.0", name, args, std::to_string(++id)}).dump();
 
 		auto self(shared_from_this());
 
@@ -62,11 +61,11 @@ private:
 	{
 		auto self(shared_from_this());
 
-        _buffer = "";
+		_buffer = "";
 
 		boost::asio::async_read(
-                _socket, boost::asio::dynamic_buffer(_buffer), boost::asio::transfer_at_least(1),
-                [this, self, completion](boost::system::error_code ec, std::size_t /*length*/) {
+			_socket, boost::asio::dynamic_buffer(_buffer), boost::asio::transfer_at_least(1),
+			[this, self, completion](boost::system::error_code ec, std::size_t /*length*/) {
 				if (!ec)
 				{
 					auto srsp = _buffer;
@@ -90,34 +89,37 @@ private:
 class Client
 {
 private:
-    boost::asio::io_context& _ioc;
-    tcp::socket              _socket;
-public:
-    Client(boost::asio::io_context& ioc, const tcp::resolver::results_type& endpoints)
-    : _ioc(ioc)
-    , _socket(ioc)
-    {
-        boost::asio::async_connect(_socket, endpoints, [](boost::system::error_code ec, tcp::endpoint endpoint) {
-            if (ec)
-            {
-                std::cerr << "error on connect to " << endpoint << " : " << ec << std::endl;
-            }
-        });
-    }
-    ~Client()
-    {
-        close();
-    }
-    void call(const std::string& name, const Json& args, Completion completion)
-    {
-        auto session = std::make_shared<Session>(_ioc, _socket);
-        session->call(name, args, completion);
-    }
-    void close()
-    {
-        boost::asio::post(_ioc, [this]() { _socket.close(); });
-    }
+	boost::asio::io_context& _ioc;
+	tcp::socket              _socket;
 
+public:
+	Client(boost::asio::io_context& ioc, const tcp::resolver::results_type& endpoints)
+		: _ioc(ioc)
+		, _socket(ioc)
+	{
+		boost::asio::async_connect(_socket, endpoints, [](boost::system::error_code ec, tcp::endpoint endpoint) {
+			if (ec)
+			{
+				std::cerr << "error on connect to " << endpoint << " : " << ec << std::endl;
+			}
+		});
+	}
+
+	~Client()
+	{
+		close();
+	}
+
+	void call(const std::string& name, const Json& args, Completion completion)
+	{
+		auto session = std::make_shared<Session>(_ioc, _socket);
+		session->call(name, args, completion);
+	}
+
+	void close()
+	{
+		boost::asio::post(_ioc, [this]() { _socket.close(); });
+	}
 };
 
 int main(int argc, char* argv[])
@@ -146,8 +148,7 @@ int main(int argc, char* argv[])
 			std::cout << fmt::format("res={}\n", res);
 		});
 
-		client.call(
-			"unknown method", Json(), [](const ResultType& res) { std::cout << fmt::format("res={}\n", res); });
+		client.call("unknown method", Json(), [](const ResultType& res) { std::cout << fmt::format("res={}\n", res); });
 
 		io_context.run();
 		// t.join();
