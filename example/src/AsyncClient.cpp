a137ff65b430355c16c0b94e5586e195628b9bdb
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2020-06-24 23:17:04 +0200

    dispatch response by id

--- example/src/AsyncClient.cpp
+++ example/src/AsyncClient.cpp
@@ -11,13 +11,37 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <variant>
 
 using boost::asio::ip::tcp;
 using namespace jsonrpc;
 using namespace jsonrpc::rpc;
 
-using ResultType = Json;
-using Completion = std::function<void(const ResultType&)>;
+using ResponseType = std::variant<Json, Error>;
+//using ResponseType = Json;
+using Completion = std::function<void(const ResponseType&)>;
+using IdType     = Json;
+using Dispatcher = std::map<IdType, Completion>;
+
+std::ostream& operator<<(std::ostream& out, const Completion& data)
+{
+	return out << "Cpl";
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& data) {
+    std::copy(std::begin(data), std::end(data), std::ostream_iterator<T>(out, "\n"));
+}
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& out, const std::map<T, U>& data)
+{
+	//std::copy(std::begin(data), std::end(data), std::ostream_iterator<T>(out, "\n"));
+	std::for_each(std::begin(data), std::end(data), [&out](const std::pair<const T, U>& element) {
+		out << element.first << ":" << element.second << std::endl;
+	});
+	return out;
+}
 
 class Session : public std::enable_shared_from_this<Session>
 {
@@ -30,32 +54,37 @@ public:
 		: _socket(socket)
 	{}
 
-	void call(const std::string& name, const Json& args, Completion completion)
+	IdType call(const std::string& name, const Json& args, const Dispatcher& dispatcher)
 	{
-		static int id = 0;
+		static int idVal = 0;
 
-		_buffer = Json(Request{"2.0", name, args, std::to_string(++id)}).dump();
+		IdType id = std::to_string(++idVal);
+
+		// todo:: schedule timeout
+
+		_buffer = Json(Request{"2.0", name, args, id}).dump();
 
 		auto self(shared_from_this());
 
 		boost::asio::async_write(
 			_socket, boost::asio::buffer(_buffer),
-			[this, self, completion](boost::system::error_code ec, std::size_t length) {
+			[this, self, &dispatcher](boost::system::error_code ec, std::size_t length) {
 				if (!ec)
 				{
 					std::cout << fmt::format("{} <<< {} length={}\n", util::ts(), _buffer, length);
 
-					receive(completion);
+					receive(dispatcher);
 				}
 				else
 				{
 					std::cerr << "error on call: " << _buffer << " code: " << ec << " length: " << length << std::endl;
 				}
 			});
+		return id;
 	}
 
 private:
-	void receive(Completion completion)
+	void receive(const Dispatcher& dispatcher)
 	{
 		auto self(shared_from_this());
 
@@ -63,23 +92,32 @@ private:
 
 		boost::asio::async_read(
 			_socket, boost::asio::dynamic_buffer(_buffer), boost::asio::transfer_at_least(1),
-			[this, self, completion](boost::system::error_code ec, std::size_t /*length*/) {
+			[this, self, &dispatcher](boost::system::error_code ec, std::size_t /*length*/) {
 				if (!ec)
 				{
-					// std::cout << _buffer << std::endl;
-
 					util::for_each_split(_buffer, "}{", [&](const std::string& srsp) {
 						std::cout << fmt::format("{} >>> {}\n", util::ts(), srsp);
-						auto rsp = Response(Json::parse(srsp));
 
-						// todo: dispatch correctly by id
+						auto rsp = Response(Json::parse(srsp));
+						if (rsp.id == Json({}))
+						{
+							throw std::runtime_error("invalid id: " + rsp.id.dump());
+						}
+
+						auto it = dispatcher.find(rsp.id);
+						if (it == dispatcher.end())
+						{
+							throw std::runtime_error("invalid id: " + rsp.id.dump());
+						}
+						auto completion = it->second;
+
 						if (rsp.result)
 						{
-							completion(Json(*rsp.result));
+							completion(*rsp.result);
 						}
 						else if (rsp.error)
 						{
-							completion(Json(*rsp.error));
+							completion(*rsp.error);
 						}
 					});
 				}
@@ -92,6 +130,7 @@ class Client
 private:
 	boost::asio::io_context& _ioc;
 	tcp::socket              _socket;
+	Dispatcher               _dispatcher;
 
 public:
 	Client(boost::asio::io_context& ioc, const tcp::resolver::results_type& endpoints)
@@ -114,7 +153,8 @@ public:
 	void call(const std::string& name, const Json& args, Completion completion)
 	{
 		auto session = std::make_shared<Session>(_ioc, _socket);
-		session->call(name, args, completion);
+		auto id      = session->call(name, args, _dispatcher);
+		_dispatcher.emplace(id, completion);
 	}
 
 	void close()
@@ -139,17 +179,16 @@ int main(int argc, char* argv[])
 		auto          endpoints = resolver.resolve(argv[1], argv[2]);
 
 		// std::thread t([&io_context]() { io_context.run(); });
+		auto ResponsePrinter = [](const ResponseType& rsp) {
+			std::string res = std::holds_alternative<Error>(rsp) ? to_string(std::get<Error>(rsp))
+																 : std::get<Json>(rsp).dump();
+			std::cout << res << std::endl;
+		};
 
 		auto client = Client(io_context, endpoints);
-		client.call("foo", Json::parse(R"([42, 23])"), [](const ResultType& res) {
-			std::cout << fmt::format("res={}\n", res);
-		});
-
-		client.call("bar", Json::parse(R"("params")"), [](const ResultType& res) {
-			std::cout << fmt::format("res={}\n", res);
-		});
-
-		client.call("unknown method", Json(), [](const ResultType& res) { std::cout << fmt::format("res={}\n", res); });
+		client.call("foo", Json::parse(R"([42, 23])"), ResponsePrinter);
+		client.call("bar", Json::parse(R"("params")"), ResponsePrinter);
+		client.call("unknown method", Json(), ResponsePrinter);
 
 		io_context.run();
 		// t.join();
