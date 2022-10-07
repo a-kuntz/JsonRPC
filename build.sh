eb68b248a81493fed775cc5091eeefa090254ae5
Tobias Jaeger <tobias.jaeger@itk-engineering.de>
2022-10-07 16:56:09 +0200

    add conan install to build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ BUILD=build
 
 mkdir -p ${BUILD}
 pushd ${BUILD}
+	conan install .. --install-folder=build -pr:b=default -s build_type=Debug
 	cmake ..
 	cmake -B build -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug .
 	ctest --verbose
