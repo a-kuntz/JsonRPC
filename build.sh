a58e458d62a4b94b1ebacb333bfa228456364b40
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2022-10-12 09:04:17 -0400

    call conan outside build folder

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 BUILD=build
 
+conan install . --install-folder=${BUILD} # -pr:b=default -s build_type=Debug
+
 mkdir -p ${BUILD}
 pushd ${BUILD}
-	conan install .. --install-folder=build -pr:b=default -s build_type=Debug
 	cmake ..
-	cmake -B build -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug .
+	cmake -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug .
 	ctest --verbose
 popd
