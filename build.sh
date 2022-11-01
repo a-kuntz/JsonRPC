c50f16bfc6c67c981bd4b71a3530e6b4fc794376
Antonio Ordóñez Müller <antonio.mueller@itk-engineering.de>
2022-11-01 21:06:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 set -euo pipefail
 
-BUILD=build
+BUILD_FOLDER=build
+BUILD_TYPE=Release
 
-mkdir -p ${BUILD}
-pushd ${BUILD}
-	cmake ..
-	cmake --build .
-	ctest --verbose
-popd
+conan install . --install-folder=${BUILD_FOLDER} -pr:b=default -s build_type=${BUILD_TYPE}
+cmake -B ${BUILD_FOLDER} -DCMAKE_TOOLCHAIN_FILE=${BUILD_FOLDER}/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake --build ${BUILD_FOLDER}
+cd ${BUILD_FOLDER}; ctest --verbose; cd ..
