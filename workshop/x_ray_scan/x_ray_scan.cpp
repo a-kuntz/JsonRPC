13460269a5433bc163d1f84cb09755a44ec333ac
t-jaeger09 <115138192+t-jaeger09@users.noreply.github.com>
2022-11-02 14:58:15 +0100

    Delete workshop/x_ray_scan directory

--- workshop/x_ray_scan/x_ray_scan.cpp
+++
@@ -1,5 +0,0 @@
-#include <jsonrpc/net/ServerTransport.h>
-#include <jsonrpc/rpc/Dispatcher.h>
-
-#include "x_ray_scan.h"
-using namespace jsonrpc;
--- workshop/x_ray_scan/x_ray_scan.h
+++
@@ -1,7 +0,0 @@
-#ifndef X_RAY_SCAN_H
-#define X_RAY_SCAN_H
-
-
-
-
-#endif /* X_RAY_SCAN_H */
