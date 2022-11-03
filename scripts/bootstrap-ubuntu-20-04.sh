d4ae55e13f947642f5e3fda2a8119b050258bbab
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2022-11-03 05:57:49 -0400

    install ccache

--- scripts/bootstrap-ubuntu-20-04.sh
+++ scripts/bootstrap-ubuntu-20-04.sh
@@ -3,18 +3,24 @@
 sudo apt update
 
 sudo apt install -y \
+	ccache \
 	cmake \
 	cmake-curses-gui \
 	git \
 	git-gui \
 	gitk \
 	python3-pip
-
 sudo snap install code --classic
 
 pip3 install conan
 conan config init
 conan profile update settings.compiler.libcxx=libstdc++11 default
+
+if ! $(grep -q ccache ~/.bashrc)
+then
+	echo 'export PATH=/usr/lib/ccache:${PATH}' >> ~/.bashrc
+	source ~/.bashrc
+fi
 
 if ! $(grep -q .local/bin ~/.bashrc)
 then
