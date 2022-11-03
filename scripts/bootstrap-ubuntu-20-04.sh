a8b129b794184ef2a7ec6216cb75846ac5e21326
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2022-11-03 05:57:37 -0400

    fix PATH logic

--- scripts/bootstrap-ubuntu-20-04.sh
+++ scripts/bootstrap-ubuntu-20-04.sh
@@ -16,8 +16,8 @@ pip3 install conan
 conan config init
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
-if $(grep -q .local/bin ~/.bashrc)
+if ! $(grep -q .local/bin ~/.bashrc)
 then
-	echo 'PATH=${PATH}:${HOME}/.local/bin' >> ~/.bashrc
+	echo 'export PATH=${PATH}:${HOME}/.local/bin' >> ~/.bashrc
 	source ~/.bashrc
 fi
