526234c36018735bef06e21ef17dea2fe8863916
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2022-11-05 00:39:33 +0100

    rename Client/Server to SampleClient/Server

---
+++ example/src/SampleClient.cpp
@@ -0,0 +1,42 @@
+#include <jsonrpc/Config.h>
+#include <jsonrpc/net/ClientTransport.h>
+#include <jsonrpc/rpc/Client.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+using boost::asio::ip::tcp;
+using namespace jsonrpc;
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 3)
+		{
+			std::cerr << "Usage: " << argv[0] << " <host> <port>\n";
+			return 1;
+		}
+
+		boost::asio::io_context io_context;
+
+		net::ClientTransport transport(io_context);
+		transport.connect(argv[1], atoi(argv[2]));
+
+		auto client = rpc::Client(transport);
+		client.call("foo", {"arg1", "arg2", "arg3"});
+		client.call("bar", "params");
+		client.call("set-value", 42);
+		client.call("get-value", {});
+		client.call("echo", {{"a", 1}, {"b", 2}, {"c", {1, 2, 3}}});
+		client.call("unknown method", {});
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
