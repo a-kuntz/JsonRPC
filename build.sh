f8a1f9b349ae2d73442fa1ed72eb47b580ef8440
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2022-11-15 08:28:15 -0500

    build with tests and examples by default

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ BUILD_FOLDER=build
 BUILD_TYPE=Release
 
 conan install . --install-folder=${BUILD_FOLDER} -pr:b=default -s build_type=${BUILD_TYPE}
-cmake -B ${BUILD_FOLDER} -DCMAKE_TOOLCHAIN_FILE=${BUILD_FOLDER}/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -B ${BUILD_FOLDER} -DCMAKE_TOOLCHAIN_FILE=${BUILD_FOLDER}/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON -DBUILD_EXAMPLE=ON
 cmake --build ${BUILD_FOLDER}
 cd ${BUILD_FOLDER}; ctest --verbose; cd ..
