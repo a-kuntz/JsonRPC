17fab7dee0ab7906f00fd31777960db7b780b83e
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2023-01-18 01:20:29 +0100

    distinguish debug/release build folder

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -x
 set -euo pipefail
 
-BUILD_FOLDER=build
 BUILD_TYPE=${1:-Release}
+BUILD_FOLDER=build-${BUILD_TYPE}
 
 conan install . --install-folder=${BUILD_FOLDER} -pr:b=default -s build_type=${BUILD_TYPE} --build=missing
 cmake -B ${BUILD_FOLDER} -DCMAKE_TOOLCHAIN_FILE=${BUILD_FOLDER}/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
